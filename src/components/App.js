02c549657ca6f9b1b1b6ec23f81076c30d4f1fe8
Oana <oana.anghelachi@gmail.com>
2021-05-18 17:24:51 +0200

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import {Button} from 'components/common'
+import Button from 'components/common/Button'
+
+function App() {
+  return (
+    <div>
+    <h1>
+      App
+      
+    </h1>
+    <h2>button</h2>
+    <Button>test</Button>
+    </div>
+  );
+}
+
+export default App;
