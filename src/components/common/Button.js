02db222b880d9fe65af01a3eb548deedd51696d2
Oana <oana.anghelachi@gmail.com>
2021-06-17 14:10:15 +0300

    Add btns and loading/submit state to form

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -2,7 +2,7 @@ import styled, {css} from 'styled-components'
 
 const Button = styled.button`
     color: white; 
-    background: ${props => props.secondary ? 'pink' : '#f8049c'}; 
+    background: ${props => props.secondary ? '#fdd54f' : '#f8049c'}; 
     font-weight: bold; 
     ${props => props.large ? css`
         padding: 10px; 
