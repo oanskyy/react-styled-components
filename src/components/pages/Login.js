7781d9394307824b8518a7fcdcacda89f672a532
Oana <oana.anghelachi@gmail.com>
2021-09-24 19:14:11 +0200

    Add useState on Form

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react' 
+import React, {useState} from 'react' 
 import {PageLayout} from 'components/common/PageLayout'
 import styled from 'styled-components'
 import { Input } from '../common'
@@ -12,12 +12,14 @@ const Form = styled.form`
 `
 
 export default function Login() { 
+    const [formFields, setFormFields] = useState({username: '', password: ''})
+
     return ( 
         <PageLayout>
             <h1>login from login.js</h1>
             <p>Chocolate cotton candy biscuit marzipan chocolate bar sweet shortbread cake gummies. Icing cheesecake biscuit candy marshmallow cake sugar plum. Jelly-o macaroon cheesecake jelly chocolate soufflé shortbread. Sesame snaps cupcake gummies croissant topping marshmallow brownie candy canes bear claw. Shortbread brownie jujubes marshmallow biscuit candy canes gummies.</p>
             <Form>
-                <Input name="username" placeholder="Username" />
+                <Input type="" value={formFields.username} onChange={handleInputChange} name="username" placeholder="Username" />
             </Form>
         </PageLayout>
     )
