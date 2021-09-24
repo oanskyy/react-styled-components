65674d5c2f8c2b980327e9b9b871a0375cc85241
Oana <oana.anghelachi@gmail.com>
2021-09-24 13:40:35 +0200

    Redo button component

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,14 +1,22 @@
 import styled, {css} from 'styled-components';
 
 const Button = styled.button`
+    display: inline-block; 
+    padding: 16px 32px; 
+    border-radius: 28px; 
+    box-shadow: none; 
+    border: none; 
     color: white; 
+
     /* background: #f8049c;  */
     /* this is where we can interpolate a function. using the $ {}  we get access to the props.  */
     background: ${props => props.secondary ? props.theme.secondaryColor : props.theme.primaryColor}; 
+    cursor: pointer;
+    font-size: 1em; 
     font-weight: bold; 
     ${props => props.large ? css`
         padding: 10px; 
-        border-radius: 5px; 
+        /* border-radius: 5px;  */
         font-size: 1.5em;
         ` : css`
         padding: 8px; 
@@ -16,20 +24,19 @@ const Button = styled.button`
         font-size: 1em; 
         `
     };
-    padding: 8px; 
-    border-radius: 4px; 
-    box-shadow: none; 
-    font-size: 1em; 
-    border: none; 
-    /* width: 100%;  */
-    display: block; 
-    white-space: none;
     text-align: center;
+    white-space: none;
+    
 
     /* this is an attribute that we are passing in, not a prop in the traditional sense, we have this disabled state */
     &:disabled{ 
         background: #eee; 
         color: #666;
+    }
+    &:hover { 
+        background-color: #f8319a;
+        position:relative;
+	    top:3px;
     }
 `;
 
