70f908b562c34f29e293e5f70b7a4eb2efb69bf7
Oana <oana.anghelachi@gmail.com>
2021-09-24 17:52:45 +0200

    Create 'Header' component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,31 +1,38 @@
 import React, {useState} from 'react' 
 import styled from 'styled-components'
 import {Link as ReactRouterDomLink, useLocation } from 'react-router-dom' 
+// import {Toggle} from './Toggle'
 
 const HeaderWrapper = styled.header`
-    height: 60px; 
-    width: 100%; 
-    box-sizing: border-box; 
-    display: flex;
-    flex-direction: column;
     position: fixed; 
     top: 0; 
+    height: 60px; 
+    width: 100%; 
+    display: flex;
+    flex-direction: column;
     padding: 0 16px; 
     background-image: linear-gradient(to right, ${p => p.theme.primaryColor}, ${p => p.theme.secondaryColor});
     border-bottom: 3px solid ${p => p.theme.secondaryColor};
 `
 
-const Menu = styled.nav`
-    display: ${p => p.open ? 'block' : 'none'}; 
+const Menu2 = styled.nav`
+    /* display: ${p => p.open ? 'block' : 'none'};  */
+    display: flex; 
     position: relative; 
-    width: initial; 
-    border-bottom: 3px solid ${p => p.theme.secondaryColor}; 
-    margin: auto 0 auto auto; 
-    font-family: 'Open Sans'; 
-    background: none; 
+    /* width: initial; 
     left: initial; 
-    top: initial;
+    top: initial; */
+    /* border-bottom: 3px solid ${p => p.theme.secondaryColor};  */
+    margin: auto 0 auto auto; 
+    
 `
+
+const Menu = styled.nav`
+    display: flex; 
+    justify-content: flex-end;
+`
+
+// destructure from the PROPS the 'isActive' prop, the childred and we use the spread operator '...' for any other props
 const Link = ({isActive, children, ...props}) => {
     return (
         <ReactRouterDomLink {...props}>
@@ -34,13 +41,11 @@ const Link = ({isActive, children, ...props}) => {
     );
 };
 
-
 const StyledLink = styled(Link)`
-    padding: 4px 8px;
     display: block;
+    padding: 4px 8px;
     text-align: center;
-    box-sizing: border-box;
-    margin: auto 0;
+    text-decoration: none;
     font-weight: ${p => p.isActive ? 'bold' : 'normal'};
     color: ${p => p.theme.bodyFontColor};
 `
@@ -76,9 +81,23 @@ export function Header(){
                 <div />
                 <div />
             </MobileMenuIcon>
-            <Menu open={menuOpen}>
-                <StyledLink to="/login" isActive={pathname === '/login'}>Login</StyledLink>
-                <StyledLink to="/" isActive={pathname === '/'}> Home </StyledLink>
+            <Menu2 open={menuOpen}>
+                <StyledLink to="/" isActive={pathname === '/'}> 
+                Home 
+                </StyledLink>
+                <StyledLink to="/login" isActive={pathname === '/login'}>
+                Login
+                </StyledLink>
+                
+                {/* <Toggle></Toggle> */}
+            </Menu2>
+            <Menu>
+            <StyledLink to="/" isActive={pathname === '/'}> 
+                Home 
+            </StyledLink>
+            <StyledLink to="/login" isActive={pathname === '/login'}>
+                Login
+            </StyledLink>
             </Menu>
         </HeaderWrapper>
     )
