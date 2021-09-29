1f01e1064285eda9207b5a0fa92df84b01cf9933
Oana <oana.anghelachi@gmail.com>
2021-09-29 13:39:02 +0200

    Implement the 'show/hide' password toggle

--- src/components/common/PasswordInput.js
+++ src/components/common/PasswordInput.js
@@ -1,9 +1,17 @@
-import React from "react"; 
+import React, {useState} from "react"; 
 import styled from "styled-components"; 
 import { Input } from "./Input";
 
+const PasswordInputWrapper = styled.div`
+    display: flex; 
+
+    ~div { 
+        margin-bottom: 8px;
+    }
+`
+
 // this '.attrs()' is a function which takes in a callback function. from this callback function we want to return an OBJ{}
-const PasswordInoutStyled = styled(Input).attrs(()=> ({ 
+const PasswordInputStyled = styled(Input).attrs(()=> ({ 
     type: 'password' ,
     placeholder: 'Password' 
 }))`
@@ -11,10 +19,37 @@ border-top-right-radius: 0;
 border-bottom-right-radius: 0;
 `
 
+const ToggleButton = styled.div`
+    /* height: 40px; */
+    display: flex; 
+    padding: 8px; 
+    border: 1px solid #ccc; 
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px; 
+    border-left: 0; 
+    background: white; 
+    font-size: 0.9em; 
+    font-weight: bold; 
+    cursor: pointer; 
+    user-select: none; 
+`
+
 export function PasswordInput(props) { 
-    // const [showPassword, setShowPassword] = useState(false)
+    const [showPassword, setShowPassword] = useState(true)
 
     return ( 
-        <><PasswordInoutStyled {...props} /></>
+        <>
+            <PasswordInputWrapper>
+                <PasswordInputStyled {...props} />
+                <ToggleButton onClick={() => setShowPassword(s => !s)}> 
+                
+                    {showPassword ? 'Hide' : 'Show'} 
+                </ToggleButton>
+            </PasswordInputWrapper>
+            
+            <div>
+                {showPassword ? props.value : ''}
+            </div>
+        </>
     )
 }
